bb30e8f96047ba4c99611498ce4e5c34aa429eba
ngohipha <83013006+ngohipha@users.noreply.github.com>
2024-05-30 19:03:59 +0700

    vnpay
     vnpay

---
+++ be/checkoutonline.js
@@ -0,0 +1,128 @@
+// tạo giao dịch thanh toán 
+router.post('/create_payment_url', function (req, res, next) {
+    var ipAddr = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+
+    var config = require('config');
+    var dateFormat = require('dateformat');
+
+    let config = require("config");
+
+    
+    var tmnCode = config.get('vnp_TmnCode');
+    var secretKey = config.get('vnp_HashSecret');
+    var vnpUrl = config.get('vnp_Url');
+    var returnUrl = config.get('vnp_ReturnUrl');
+
+    var date = new Date();
+
+    var createDate = dateFormat(date, 'yyyymmddHHmmss');
+    // không dùng date dùng req.body.orderId hoặc dùng sdk zalo thì truyền taọ orderId ra truyền vào 
+    var orderId = dateFormat(date, 'HHmmss');
+    var amount = req.body.amount;
+    var bankCode = req.body.bankCode;
+    
+    var orderInfo = req.body.orderDescription;
+    var orderType = req.body.orderType;
+    var locale = req.body.language;
+    if(locale === null || locale === ''){
+        locale = 'vn';
+    }
+    var currCode = 'VND';
+    var vnp_Params = {};
+    vnp_Params['vnp_Version'] = '2.1.0';
+    vnp_Params['vnp_Command'] = 'pay';
+    vnp_Params['vnp_TmnCode'] = tmnCode;
+    // vnp_Params['vnp_Merchant'] = ''
+    vnp_Params['vnp_Locale'] = locale;
+    vnp_Params['vnp_CurrCode'] = currCode;
+    vnp_Params['vnp_TxnRef'] = orderId;
+    vnp_Params["vnp_OrderInfo"] = "Thanh toan cho ma GD:" + orderId;
+    vnp_Params['vnp_OrderType'] = orderType;
+    vnp_Params['vnp_Amount'] = amount * 100;
+    // thanh toán trả về lưu vào cơ sỡ dữ liệu vd: localhost:3000/thanks
+    vnp_Params['vnp_ReturnUrl'] = returnUrl;
+    vnp_Params['vnp_IpAddr'] = ipAddr; // n sẽ lấy địa chỉ host 
+    vnp_Params['vnp_CreateDate'] = createDate;
+    if(bankCode !== null && bankCode !== ''){
+        vnp_Params['vnp_BankCode'] = bankCode;
+    }
+
+    vnp_Params = sortObject(vnp_Params);
+
+    var querystring = require('qs');
+    var signData = querystring.stringify(vnp_Params, { encode: false });
+    var crypto = require("crypto");     
+    var hmac = crypto.createHmac("sha512", secretKey);
+    var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
+    vnp_Params['vnp_SecureHash'] = signed;
+    vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+
+    res.redirect(vnpUrl)
+});
+// API hoàn tiền giao dịch
+router.get('/vnpay_return', function (req, res, next) {
+    var vnp_Params = req.query;
+
+    var secureHash = vnp_Params['vnp_SecureHash'];
+
+    delete vnp_Params['vnp_SecureHash'];
+    delete vnp_Params['vnp_SecureHashType'];
+
+    vnp_Params = sortObject(vnp_Params);
+
+    var config = require('config');
+    var tmnCode = config.get('vnp_TmnCode');
+    var secretKey = config.get('vnp_HashSecret');
+
+    var querystring = require('qs');
+    var signData = querystring.stringify(vnp_Params, { encode: false });
+    var crypto = require("crypto");     
+    var hmac = crypto.createHmac("sha512", secretKey);
+    var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");     
+
+    if(secureHash === signed){
+        //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
+
+        res.render('success', {code: vnp_Params['vnp_ResponseCode']})
+    } else{
+        res.render('success', {code: '97'})
+    }
+});
+//API truy vấn kết quả thanh toán
+router.get('/vnpay_ipn', function (req, res, next) {
+    var vnp_Params = req.query;
+    var secureHash = vnp_Params['vnp_SecureHash'];
+
+    delete vnp_Params['vnp_SecureHash'];
+    delete vnp_Params['vnp_SecureHashType'];
+
+    vnp_Params = sortObject(vnp_Params);
+    var config = require('config');
+    var secretKey = config.get('vnp_HashSecret');
+    var querystring = require('qs');
+    var signData = querystring.stringify(vnp_Params, { encode: false });
+    var crypto = require("crypto");     
+    var hmac = crypto.createHmac("sha512", secretKey);
+    var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");     
+     
+
+    if(secureHash === signed){
+        var orderId = vnp_Params['vnp_TxnRef'];
+        var rspCode = vnp_Params['vnp_ResponseCode'];
+        //Kiem tra du lieu co hop le khong, cap nhat trang thai don hang va gui ket qua cho VNPAY theo dinh dang duoi
+        res.status(200).json({RspCode: '00', Message: 'success'})
+        //thanh cong
+         //paymentStatus = '1'
+         // Ở đây cập nhật trạng thái giao dịch thanh toán thành công vào CSDL của bạn
+
+    }
+    else {
+        res.status(200).json({RspCode: '97', Message: 'Fail checksum'})
+         //that bai
+         //paymentStatus = '2'
+         // Ở đây cập nhật trạng thái giao dịch thanh toán thất bại vào CSDL của bạn
+    }
+});
